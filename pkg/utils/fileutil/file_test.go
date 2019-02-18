2c8e8cfbfe077a839ca948578ee0e62820a4af1f
kbabushkin <kbabushkin@hotmail.com>
2019-02-18 12:29:45 -0800

    rm collector only utils

--- pkg/utils/fileutil/file_test.go
+++ pkg/utils/fileutil/file_test.go
@@ -14,47 +14,8 @@
 package fileutil
 
 import (
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
-	"syscall"
 	"testing"
-
-	"github.com/logrange/range/pkg/utils/crypto"
-	"github.com/stretchr/testify/assert"
 )
-
-func TestGetFileId(t *testing.T) {
-	fd, err := ioutil.TempFile("/tmp", "GetFileId_")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer func() {
-		if fd != nil {
-			fd.Close()
-			os.Remove(fd.Name())
-		}
-	}()
-
-	fi, err := fd.Stat()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	id := GetFileId(fd.Name(), fi)
-	pp := strings.Split(id, "_")
-
-	hsh := pp[0]
-	ino, _ := strconv.ParseUint(pp[1], 10, 64)
-	dev, _ := strconv.ParseUint(pp[2], 10, 32)
-
-	assert.Equal(t, len(pp), 3)
-	assert.Equal(t, hsh, crypto.Md5(fd.Name()))
-	assert.Equal(t, ino, fi.Sys().(*syscall.Stat_t).Ino)
-	assert.Equal(t, uint64(dev), uint64(fi.Sys().(*syscall.Stat_t).Dev))
-}
 
 func TestSetFileExt(t *testing.T) {
 	if s := SetFileExt("/a/b/c.ddd", ".idx"); "/a/b/c.idx" != s {
